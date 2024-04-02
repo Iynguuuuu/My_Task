3d810f49ce26daa63a782d1f06a3eed50cd647bb
Iynguuuuu <140171430+Iynguuuuu@users.noreply.github.com>
2024-04-02 12:08:28 +0530

    made CRUD todos functions

--- api/server.js
+++ api/server.js
@@ -10,4 +10,54 @@ app.use(cors());
 mongoose.connect("mongodb://localhost:27017/my_task" ,{
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log("Connected to MongoDB")).catch(console.error);
+})
+   .then(() => console.log("Connected to MongoDB"))
+   .catch(console.error);
+
+
+// Models
+const Todo = require('./models/Todo');
+
+app.get('/todos', async (req, res) => {
+	const todos = await Todo.find();
+
+	res.json(todos);
+});
+
+app.post('/todo/new', (req, res) => {
+	const todo = new Todo({
+		text: req.body.text
+	})
+
+	todo.save();
+
+	res.json(todo);
+});
+
+app.delete('/todo/delete/:id', async (req, res) => {
+	const result = await Todo.findByIdAndDelete(req.params.id);
+
+	res.json({result});
+});
+
+app.get('/todo/complete/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
+
+	todo.complete = !todo.complete;
+
+	todo.save();
+
+	res.json(todo);
+})
+
+app.put('/todo/update/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
+
+	todo.text = req.body.text;
+
+	todo.save();
+
+	res.json(todo);
+});
+
+app.listen(3001, () => console.log("Server started on port 3001"));
